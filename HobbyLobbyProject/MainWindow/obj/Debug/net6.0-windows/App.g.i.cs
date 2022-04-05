7d52a7783964648a2201588b75b973d80f5a6791
meganbagrosky <78190459+meganbagrosky@users.noreply.github.com>
2022-04-05 12:50:55 -0500

    TruckerViewGUI

--- HobbyLobbyProject/MainWindow/obj/Debug/net6.0-windows/App.g.i.cs
+++ HobbyLobbyProject/MainWindow/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace MainWindow {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace MainWindow {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public static void Main() {
             MainWindow.App app = new MainWindow.App();
             app.InitializeComponent();
