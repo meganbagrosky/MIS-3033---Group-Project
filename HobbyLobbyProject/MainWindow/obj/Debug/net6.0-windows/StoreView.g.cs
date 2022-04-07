d4dd72c427f4c5fe4ca5c2d295a86b833d1d64e8
Gabriel Hernandez <Gabriel.Hernandez-1@ou.edu>
2022-04-07 09:51:02 -0500

    Store Class

--- HobbyLobbyProject/MainWindow/obj/Debug/net6.0-windows/StoreView.g.cs
+++ HobbyLobbyProject/MainWindow/obj/Debug/net6.0-windows/StoreView.g.cs
@@ -47,13 +47,13 @@ namespace MainWindow {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MainWindow;component/storeview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MainWindow;V1.0.0.0;component/storeview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\StoreView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace MainWindow {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
