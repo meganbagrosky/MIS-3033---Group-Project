1246e480978f16ab1eed9e8f5c6c39e28544320a
Gabriel Hernandez <Gabriel.Hernandez-1@ou.edu>
2022-04-13 20:16:50 -0500

    DOne

---
+++ MVC-Participation/MVC-Beginner/Models/PokemonAPI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Beginner.Models
+{
+    public class PokemonAPi
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public string previous { get; set; }
+        public List<Pokemon> results { get; set; }
+    }
+
+    public class Pokemon
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+}
---
+++ MVC-Participation/MVC-Beginner/Models/PokemonInfoAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Beginner.Models
+{
+    public class PokemonInfoAPI
+    {
+        //weight, height and then their images (sprites).
+        public int height { get; set; }
+        public string name { get; set; }
+        public int weight { get; set; }
+        public sprite sprites { get; set; }
+    }
+
+    public class sprite
+    {
+        public string back_default { get; set; }
+        public string front_default { get; set; }
+        public sprite()
+        {
+            back_default = string.Empty;
+            front_default = string.Empty;
+        }
+        //public override string ToString()
+        //{
+        //    return $"{front_default}";
+        //}
+
+    }
+}
