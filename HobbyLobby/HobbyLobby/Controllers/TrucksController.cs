01e61aced1eab6d153ad68ed7400578e7a4391f5
meganbagrosky <78190459+meganbagrosky@users.noreply.github.com>
2022-04-23 20:19:10 -0500

    update

---
+++ HobbyLobby/HobbyLobby/Controllers/TrucksController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HobbyLobby.Models;
+
+namespace HobbyLobby.Controllers
+{
+    public class TrucksController : Controller
+    {
+        private DB_128040_hobbylobbyEntities db = new DB_128040_hobbylobbyEntities();
+
+        // GET: Trucks
+        public ActionResult Index()
+        {
+            return View(db.Trucks.ToList());
+        }
+
+        // GET: Trucks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Truck truck = db.Trucks.Find(id);
+            if (truck == null)
+            {
+                return HttpNotFound();
+            }
+            return View(truck);
+        }
+
+        // GET: Trucks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trucks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TruckNumber,Capacity")] Truck truck)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trucks.Add(truck);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(truck);
+        }
+
+        // GET: Trucks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Truck truck = db.Trucks.Find(id);
+            if (truck == null)
+            {
+                return HttpNotFound();
+            }
+            return View(truck);
+        }
+
+        // POST: Trucks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TruckNumber,Capacity")] Truck truck)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(truck).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(truck);
+        }
+
+        // GET: Trucks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Truck truck = db.Trucks.Find(id);
+            if (truck == null)
+            {
+                return HttpNotFound();
+            }
+            return View(truck);
+        }
+
+        // POST: Trucks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Truck truck = db.Trucks.Find(id);
+            db.Trucks.Remove(truck);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
